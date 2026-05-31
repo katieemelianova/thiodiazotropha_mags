e65a30aa395e34b89989aa9d418ff5d07ee35cce
Katie Emelianova <katieemelianova@Mac.telekom.ip>
2026-05-31 21:17:29 +0200

    add more bits

--- kofam_all_genes.R
+++ kofam_all_genes.R
@@ -7,26 +7,55 @@ kofam <- vroom(f, delim = "\t", id = "assembly") %>%
   mutate(assembly = str_split_i(assembly, "/", 8) %>% str_replace("_kofam", "") %>% paste0(".1"))
 
 
-kofam_genome <- left_join(kofam, genome_all, by="assembly") 
-#%>% filter(significant == "*")
+kofam_genome <- left_join(kofam, genome_all, by="assembly") %>% filter(significant == "*" & study != "gtdb")
 
 
 kofam_genome %>% group_by(study) %>% summarise(length(unique(assembly)))
 
 
-kofam_genome$KO_definition
+
+########################################
+#       CLAM more than PLANT           #
+########################################
+
+kofam_genome %>%
+  group_by(KO_definition, study) %>%
+  summarise(test=n()) %>%
+  pivot_wider(names_from = study, values_from = test, values_fill = 0) %>%
+  mutate(huang_percent = (huang/10) * 100,
+         rolando_percent = (rolando/10) * 100,
+         osvatic_percent = (osvatic/29) * 100,
+         giani_percent = (giani/3) * 100) %>%
+  filter(huang_percent < 10 & rolando_percent < 10 & osvatic_percent > 70 & giani_percent > 70) %>%
+  data.frame()
+
+
+########################################
+#       PLANT more than CLAM           #
+########################################
+
+kofam_genome %>%
+  group_by(KO_definition, study) %>%
+  summarise(test=n()) %>%
+  pivot_wider(names_from = study, values_from = test, values_fill = 0) %>%
+  mutate(huang_percent = (huang/10) * 100,
+         rolando_percent = (rolando/10) * 100,
+         osvatic_percent = (osvatic/29) * 100,
+         giani_percent = (giani/3) * 100) %>%
+  filter(huang_percent > 80 & rolando_percent > 80 & osvatic_percent < 5 & giani_percent < 5) %>%
+  data.frame()
+
+
+
 
 
 kofam_genome %>%
   group_by(KO_definition, study) %>%
   summarise(test=n()) %>%
   pivot_wider(names_from = study, values_from = test, values_fill = 0) %>%
-  mutate(gtdb_percent = (gtdb/13) * 100,
-         huang_percent = (huang/10) * 100,
-         rolando_percent = (rolando/10) * 100,
-         osvatic_percent = (osvatic/3) * 100) %>%
-  filter(huang_percent < 10 & rolando_percent < 10 & osvatic_percent > 70) %>%
-  data.frame()
+  filter(KO_definition == "MFS transporter, DHA2 family, tetracycline/oxytetracycline resistance protein")
 
 
+# KO definition most similar to Otr which was found to be 
+#"MFS transporter, DHA2 family, tetracycline/oxytetracycline resistance protein"
 
