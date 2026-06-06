27be42c92dcfe30f5d894c83870a8fa77951a7cc
Katie Emelianova <katieemelianova@Katies-MacBook-Air-3.local>
2026-06-06 20:44:27 +0200

    tidying up

--- gtdb_classify.R
+++ gtdb_classify.R
@@ -20,7 +20,17 @@ osvatic_genomes <- read.table("osvatic_mag_accessions.txt") %>% pull(V1)
 giani_genomes <- read.table("giani_mag_accessions.txt") %>% pull(V1)
 morel_genomes <- read.table("morel_mag_accessions.txt") %>% pull(V1)
 petersen_clam <- c("luna_ont_bin1", "lotti_ont_bin1")
-gtdb_genomes <- read.table("GTDB_subtree_mag_acessions.txt") %>% pull(V1)
+#gtdb_genomes <- read.table("GTDB_subtree_mag_acessions.txt") %>% pull(V1)
+
+ficus_genomes <- c("PIE_T1P1_root_Marsh_MG__3300081559_11",
+                   "PIE_T1P1_root_Marsh_MG__3300081559_s3",
+                   "PIE_T2P3_root_Marsh_MG__3300077085_6",
+                   "PIE_T3P3_root_Marsh_MG__3300077490_s15")
+
+
+
+
+
 
 
 #################################################
@@ -32,10 +42,9 @@ bac120$accession <- substring(bac120$accession, 4)
 
 # read in tree from gtdbtk
 gtdb_tree<-ape::read.tree("gtdbtk.bac120.user_msa.fasta.treefile")
-gtdb_tree_subset <- gtdb_tree
 
 
-gtdb_tree_subset$tip.label <- ifelse(startsWith(gtdb_tree_subset$tip.label, "GCA"), paste0("GCA_", str_split_i(gtdb_tree_subset$tip.label, "_", 2)), gtdb_tree_subset$tip.label)
+gtdb_tree$tip.label <- ifelse(startsWith(gtdb_tree_subset$tip.label, "GCA"), paste0("GCA_", str_split_i(gtdb_tree_subset$tip.label, "_", 2)), gtdb_tree$tip.label)
 
 # genomes start with a random string so remove these so I can match up with genome accession names
 # gtdb_tree$tip.label <- substring(gtdb_tree$tip.label, 4)
@@ -51,24 +60,32 @@ gtdb_tree_subset$tip.label <- ifelse(startsWith(gtdb_tree_subset$tip.label, "GCA
 #################################################
 
 # use the total bac120 dataset to get the genus and if available species for each tip
-gtdb_tree_subset$genus <- left_join(data.frame(accession=gtdb_tree_subset$tip.label), bac120, by="accession") %>% dplyr::select(accession, ncbi_taxonomy) %>% mutate(ncbi_taxonomy=str_split_i(ncbi_taxonomy, ";", 6)) %>% pull(ncbi_taxonomy) %>% str_replace("g__", "")
-gtdb_tree_subset$species <- left_join(data.frame(accession=gtdb_tree_subset$tip.label), bac120, by="accession") %>% dplyr::select(accession, ncbi_taxonomy) %>% mutate(ncbi_taxonomy=str_split_i(ncbi_taxonomy, ";", 7)) %>% pull(ncbi_taxonomy) %>% str_replace("s__", "")
-gtdb_tree_subset$species <- ifelse(gtdb_tree_subset$species == "", gtdb_tree_subset$genus, gtdb_tree_subset$species)
+gtdb_tree$genus <- left_join(data.frame(accession=gtdb_tree$tip.label), bac120, by="accession") %>% dplyr::select(accession, ncbi_taxonomy) %>% mutate(ncbi_taxonomy=str_split_i(ncbi_taxonomy, ";", 6)) %>% pull(ncbi_taxonomy) %>% str_replace("g__", "")
+gtdb_tree$species <- left_join(data.frame(accession=gtdb_tree$tip.label), bac120, by="accession") %>% dplyr::select(accession, ncbi_taxonomy) %>% mutate(ncbi_taxonomy=str_split_i(ncbi_taxonomy, ";", 7)) %>% pull(ncbi_taxonomy) %>% str_replace("s__", "")
+gtdb_tree$species <- ifelse(gtdb_tree$species == "", gtdb_tree$genus, gtdb_tree$species)
 
 
 
 
-# label genomes by which dataset they came from
-gtdb_tree_subset$genome <- case_when(gtdb_tree_subset$tip.label %in% rolando_genomes ~ "USA Spartina",
-                                     gtdb_tree_subset$tip.label %in% huang_genomes ~ "China Spartina",
-                                     gtdb_tree_subset$tip.label %in% osvatic_genomes ~ "Clam Osvatic",
-                                     gtdb_tree_subset$tip.label %in% giani_genomes ~ "Clam Giani",
-                                     gtdb_tree_subset$tip.label %in% morel_genomes ~ "Clam Morel",
-                                     gtdb_tree_subset$tip.label %in% petersen_clam ~ "Clam Petersen",
-                                     gtdb_tree_subset$tip.label %in% gtdb_genomes ~ "gtdb input",
-                                    !(gtdb_tree_subset$tip.label) %in% c(rolando_genomes, huang_genomes, osvatic_genomes) ~ "GTDB")
+# label genomes by which dataset and host they came from
+gtdb_tree$genome <- case_when(gtdb_tree$tip.label %in% rolando_genomes ~ "USA Spartina",
+                                     gtdb_tree$tip.label %in% huang_genomes ~ "China Spartina",
+                                     gtdb_tree$tip.label %in% osvatic_genomes ~ "Clam Osvatic",
+                                     gtdb_tree$tip.label %in% giani_genomes ~ "Clam Giani",
+                                     gtdb_tree$tip.label %in% morel_genomes ~ "Clam Morel",
+                                     gtdb_tree$tip.label %in% petersen_clam ~ "Clam Petersen",
+                                     gtdb_tree$tip.label %in% ficus_genomes ~"Ficus Spartina",
+                                     #gtdb_tree$tip.label %in% gtdb_genomes ~ "gtdb input",
+                                    !(gtdb_tree$tip.label) %in% c(rolando_genomes, huang_genomes, osvatic_genomes) ~ "GTDB")
 
 
+gtdb_tree$host <- case_when(gtdb_tree$tip.label %in% rolando_genomes ~ "Plant",
+                              gtdb_tree$tip.label %in% huang_genomes ~ "Plant",
+                              gtdb_tree$tip.label %in% osvatic_genomes ~ "Clam",
+                              gtdb_tree$tip.label %in% giani_genomes ~ "Clam",
+                              gtdb_tree$tip.label %in% morel_genomes ~ "Clam",
+                              gtdb_tree$tip.label %in% petersen_clam ~ "Clam",
+                              gtdb_tree$tip.label %in% ficus_genomes ~"Plant")
 
 ##########################################################
 #       get DTDB genome names to download on lisc        #
@@ -80,12 +97,12 @@ gtdb_tree_subset$genome <- case_when(gtdb_tree_subset$tip.label %in% rolando_gen
 #         build tree            #
 #################################
 
-dd <- data.frame(taxa=gtdb_tree_subset$tip.label,
-                 genome=gtdb_tree_subset$genome,
-                 genus=gtdb_tree_subset$genus,
-                 species=gtdb_tree_subset$species)
+dd <- data.frame(taxa=gtdb_tree$tip.label,
+                 genome=gtdb_tree$genome,
+                 genus=gtdb_tree$genus,
+                 species=gtdb_tree$species)
 
-p<-ggtree(gtdb_tree_subset, size=0.3, colour="gray60")
+p<-ggtree(gtdb_tree, size=0.3, colour="gray60")
 
 
 png("clam_spartina_tree_iqtree_accession.png", height=4300, width=2500)
@@ -122,59 +139,22 @@ dev.off()
 
 
 
-# get info from the species IDs, the presence absence of sox genes and n50 and genome lengths to select genomes to go forward with
-# ideally we also use the trees t choose a well represented selection of genomes covering all major clades
-
-read.table("genome_lengths.txt") %>% 
-  dplyr::select(V1, V6) %>% 
-  set_colnames(c("assembly", "length")) %>% 
-  mutate(assembly=str_split_i(assembly, "_", 1)) %>%
-  mutate(length=length/1000000) %>%
-  mutate(host=case_when(assembly %in% c("osvatic", "morel", "giani") ~ "clam",
-                        assembly %in% c("rolando", "morel") ~ "plant")) %>%
-  drop_na() %>%
-  filter(length < 7 & length > 2) %>%
-  ggplot(aes(x=host, y=length)) + 
-  geom_boxplot()
-
-
-genome_length <- read.table("genome_lengths.txt") %>% 
-  dplyr::select(V1, V6) %>% 
-  set_colnames(c("assembly", "length")) %>% 
-  mutate(study=str_split_i(assembly, "_", 1)) %>%
-  mutate(assembly=str_split_i(assembly, "/", 3)) %>%
-  mutate(length=length/1000000) %>%
-  mutate(host=case_when(study %in% c("osvatic", "morel", "giani") ~ "clam",
-                        study %in% c("rolando", "huang") ~ "plant"))
-
-genome_n50 <- read.table("genome_n50.txt") %>% 
-  dplyr::select(V1, V2) %>% 
-  set_colnames(c("assembly", "n50")) %>% 
-  mutate(study=str_split_i(assembly, "_", 1)) %>%
-  mutate(assembly=str_split_i(assembly, "/", 3)) %>%
-  mutate(n50=n50/1000)
 
 
 genome_quality <- read_tsv("quality_report.tsv") %>% 
-  rename("assembly"="Name") %>%
-  mutate(assembly=str_split_i(assembly, "\\.", 1)) %>%
-  mutate(assembly=paste0(assembly, ".1")) %>%
-  dplyr::select(-c(Completeness_Model_Used, Translation_Table_Used, Additional_Notes))
+  dplyr::rename("assembly"="Name") %>%
+  dplyr::select(c(assembly, Contamination, Contig_N50, Genome_Size, Completeness)) %>%
+  mutate(assembly = ifelse(startsWith(assembly, "GCA"), paste0("GCA_", str_split_i(assembly, "_", 2)), assembly)) %>%
+  left_join(data.frame(assembly=gtdb_tree$tip.label, host=gtdb_tree$host, study=gtdb_tree$genome), by="assembly")
 
 
 
-genome_all <- inner_join(genome_length, genome_n50, by="assembly") %>%
-  dplyr::select(assembly, length, study.x, host, n50) %>%
-  rename("study"="study.x") %>%
-  inner_join(genome_quality, by="assembly")
 
-
-
-genome_all %>%
-  dplyr::select(study, host, Contamination, Completeness, n50, length) %>%
+genome_quality %>%
+  dplyr::select(study, host, Contamination, Completeness, Contig_N50, Genome_Size) %>%
   data.frame() %>% 
-  filter(study %in% c("rolando", "huang")) %>%
-  ggplot(aes(x = Completeness, y = length)) + 
+  filter(study %in% c("USA Spartina", "China Spartina", "Ficus Spartina")) %>%
+  ggplot(aes(x = Completeness, y = Genome_Size)) + 
   geom_point(size=5) +
   ylab("Plant associated Ca. Thio Genome Length (Mb") +
   theme(axis.text=element_text(size=20),
@@ -188,25 +168,31 @@ genome_all %>%
   
 
 
-genome_all %>%
-  dplyr::select(study, host, Contamination, Completeness, n50, length) %>%
-  data.frame() %>% 
-  filter(study %in% c("rolando", "huang")) %>%
-  filter(Completeness > 85)
 
 
 
 
 
-genome_all %>% 
-  filter(Completeness > 85) %>%
-  pull(assembly) %>%
-  write.table("spartina_clam_mags_85completeness.txt", quote=FALSE, row.names = FALSE, col.names = FALSE)
 
-genome_all %>% 
-  filter(Completeness > 90) %>%
-  pull(assembly) %>%
-  write.table("spartina_clam_mags_90completeness.txt", quote=FALSE, row.names = FALSE, col.names = FALSE)
+#png("tree_nodes2.png", height=3000, width=4000)
+p %<+% dd + 
+  geom_tippoint(aes(color=genome, size=genome)) + 
+  scale_colour_manual(values=c("seagreen", "dodgerblue", "goldenrod1", "purple", "red", "gray70", "lightgreen")) + 
+  geom_text2(aes(subset=!isTip, label=node), hjust=-.3, size=6)
+#dev.off()
+
+p_collapsed <- p %<+% dd + 
+  geom_tippoint(aes(color=genome, size=genome)) + 
+  scale_colour_manual(values=c("seagreen", "dodgerblue", "goldenrod1", "purple", "red", "gray70", "lightgreen")) + 
+  scale_size_manual(values=c(7, 3, 3, 3, 3, 3, 7))
 
 
+p_collapsed <- scaleClade(p_collapsed, node = 448, scale = 3)
+p_collapsed <- collapse(p_collapsed, node = 380, mode = "max", fill="goldenrod1")
+p_collapsed <- collapse(p_collapsed, node = 450, mode = "max", fill="goldenrod1")
+p_collapsed <- collapse(p_collapsed, node = 583, mode = "max", fill="goldenrod1")
+
+png("test.png", height=1000, width=1000)
+p_collapsed
+dev.off()
 
--- kofam_all_genes.R
+++ kofam_all_genes.R
@@ -1,3 +1,11 @@
+library(dplyr)
+library(readr)
+library(stringr)
+library(tidytree)
+library(tidyr)
+library(ggplot2)
+library(magrittr)
+library(vroom)
 
 
 f = list.files(path = "/Users/katieemelianova/Desktop/Spartina/thiodiazotropha_mags/kofam", full.names = TRUE)
@@ -7,7 +15,7 @@ kofam <- vroom(f, delim = "\t", id = "assembly") %>%
   mutate(assembly = str_split_i(assembly, "/", 8) %>% str_replace("_kofam", "") %>% paste0(".1"))
 
 
-kofam_genome <- left_join(kofam, genome_all, by="assembly") %>% filter(significant == "*" & study != "gtdb")
+kofam_genome <- left_join(kofam, genome_all, by="assembly") %>% filter(significant == "*" & study != "gtdb" & Completeness > 85)
 
 
 kofam_genome %>% group_by(study) %>% summarise(length(unique(assembly)))
@@ -22,12 +30,14 @@ kofam_genome %>%
   group_by(KO_definition, study) %>%
   summarise(test=n()) %>%
   pivot_wider(names_from = study, values_from = test, values_fill = 0) %>%
-  mutate(huang_percent = (huang/10) * 100,
-         rolando_percent = (rolando/10) * 100,
-         osvatic_percent = (osvatic/29) * 100,
-         giani_percent = (giani/3) * 100) %>%
-  filter(huang_percent < 10 & rolando_percent < 10 & osvatic_percent > 70 & giani_percent > 70) %>%
-  data.frame()
+  mutate(huang_percent = (huang/4) * 100,
+         rolando_percent = (rolando/7) * 100,
+         osvatic_percent = (osvatic/81) * 100,
+         morel_percent = (morel/156) * 100,
+         giani_percent = (giani/15) * 100) %>%
+  filter(huang_percent < 10 & rolando_percent < 10 & osvatic_percent > 80 & giani_percent > 80 & morel > 80) %>%
+  data.frame() %>%
+  writexl::write_xlsx("clam_morethan_plant.xlsx")
 
 
 ########################################
@@ -38,12 +48,14 @@ kofam_genome %>%
   group_by(KO_definition, study) %>%
   summarise(test=n()) %>%
   pivot_wider(names_from = study, values_from = test, values_fill = 0) %>%
-  mutate(huang_percent = (huang/10) * 100,
-         rolando_percent = (rolando/10) * 100,
-         osvatic_percent = (osvatic/29) * 100,
-         giani_percent = (giani/3) * 100) %>%
-  filter(huang_percent > 80 & rolando_percent > 80 & osvatic_percent < 5 & giani_percent < 5) %>%
-  data.frame()
+  mutate(huang_percent = (huang/4) * 100,
+         rolando_percent = (rolando/7) * 100,
+         osvatic_percent = (osvatic/81) * 100,
+         morel_percent = (morel/156) * 100,
+         giani_percent = (giani/15) * 100) %>%
+  filter(huang_percent > 80 & rolando_percent > 80 & osvatic_percent < 5 & giani_percent < 5 & morel_percent < 5) %>%
+  data.frame() %>%
+  writexl::write_xlsx("plant_morethan_clam.xlsx")
 
 
 
